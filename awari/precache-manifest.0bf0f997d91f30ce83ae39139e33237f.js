dc3005a12e4ce8d81a913bdcdce24a972e39ac10
Ray Sanders <raymond.sanders@gmail.com>
2019-12-10 19:04:10 -0600

    Init

---
+++ awari/precache-manifest.0bf0f997d91f30ce83ae39139e33237f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d1612c84c3b565b0625",
+    "url": "/awari/static/css/main.8424203c.chunk.css"
+  },
+  {
+    "revision": "8d1612c84c3b565b0625",
+    "url": "/awari/static/js/main.324e5210.chunk.js"
+  },
+  {
+    "revision": "dd18d7865e2701f6ceac",
+    "url": "/awari/static/js/runtime~main.f7b81156.js"
+  },
+  {
+    "revision": "7a91d50482be3b865343",
+    "url": "/awari/static/js/2.da01ddf0.chunk.js"
+  },
+  {
+    "revision": "6486bda82223bed78cbcdf0799ea04a9",
+    "url": "/awari/static/media/AwariMainLogoTypeColor_60percent.6486bda8.png"
+  },
+  {
+    "revision": "c6ae6ab3e3a95ab4524795c3043e2eed",
+    "url": "/awari/index.html"
+  }
+];
---
+++ awari/precache-manifest.f6096d2a5bccb360b783cd9cfa1debbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfafd43bba8029847268",
+    "url": "/static/css/main.7311e9af.chunk.css"
+  },
+  {
+    "revision": "cfafd43bba8029847268",
+    "url": "/static/js/main.2d65268b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8783fe06052830af11ef",
+    "url": "/static/js/2.f99c636e.chunk.js"
+  },
+  {
+    "revision": "6486bda82223bed78cbcdf0799ea04a9",
+    "url": "/static/media/AwariMainLogoTypeColor_60percent.6486bda8.png"
+  },
+  {
+    "revision": "a6231cc4a66299be56d60c710a607c14",
+    "url": "/index.html"
+  }
+];
